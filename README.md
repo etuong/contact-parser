34cb4470bb70c3b6265d0b1afbecb8df2ba6280a
etuong <etuong@gmail.com>
2022-04-23 15:54:41 -0700

    Finished parsing the contact lists

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# contact-parser
+# Contact Parser
+
+**Problem Statement**: I have too many contacts on my phone. Many of whom I have lost touch with. But, occasionally I get a text message from a rando and I'm like "who dis be?". Every 6 months or so I would back up my contacts. What can I do with them?
+
+**Approach**: Python script to parse the contacts into an organized spreadsheet for easy lookup.
+
+**Note**: For obvious reason I am excluding my .vcf files.
+
+**Example of a Contact**
+
+- BEGIN:VCARD
+- VERSION:2.1
+- N:Wong;Becca
+- FN:Becca Wong
+- TEL;CELL:+12061234567
+- END:VCARD
--- main.py
+++ main.py
@@ -1,8 +1,35 @@
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Contact:
+    def __init__(self, name, number):
+        self.name = name
+        self.number = number
+
+    def __str__(self) -> str:
+        return f"{self.name}'s number is {self.number}"
+
+    def __eq__(self, o: object) -> bool:
+        return isinstance(o, Contact) and o.name == self.name
+
+    def __hash__(self) -> int:
+        return hash(self.name)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    number_of_files = 3
+    contacts = set()
+    name = ""
+    number = ""
+    for i in range(1, number_of_files + 1):
+        with open(f"contact{i}.vcf", 'r') as f:
+            for x in f:
+                if x.startswith("FN"):
+                    name = x.split(":", 1)[1].strip()  # Or x[x.index(":")+1:]
+                elif name and x.startswith("TEL"):
+                    number = x.split(":", 1)[1].strip()
+
+                if name and number:
+                    contacts.add(Contact(name, number))
+                    name = ""
+                    number = ""
+
+    for contact in sorted(contacts, key=lambda x: x.name):
+        print(contact)
